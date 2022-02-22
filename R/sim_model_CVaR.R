6dd2a1c1f4ce1fc80b743fd5ee8ef7ef99869c01
emmakroell <emma.ainslie.holmes@gmail.com>
2022-02-22 12:11:50 -0500

    Add sim_model_CVaR.R
    - add CVaR implementation

--- R/sim_model_CVaR.R
+++ R/sim_model_CVaR.R
@@ -1,20 +1,22 @@
-#' Simulate the stressed model
+#' Simulate the stressed model with CVaR constraints
 #'
 #' @param kappa double, compound Poisson parameter
 #' @param jump_dist RPS_dist object, contains jump size distribution
 #' @param stress_type one of "VaR", "VaR & CVaR"
 #' @param stress_parms list of parameters for stress
-#' VaR: q: stressed VaR value, c: VaR level, stress: multiplier for P-VaR
-#' CVAR: TBA
+#' VaR: q: stressed VaR value, c: VaR level, VaR_stress: multiplier for P-VaR
+#' CVAR: q: stressed VaR value, c: VaR level, s: stressed CVaR level,
+#' VaR_stress: multiplier for P-VaR, CVaR_stress: multiplier for P-CVaR
 #' @param Npaths integer, number of paths
 #' @param endtime float, when to end sim
 #' @param dt float, step size in time
 #'
 #' @return RPS_model object
 #' @export
-stressed_sim <- function(kappa, jump_dist, stress_type = "VaR",
-                         stress_parms = list(c=c,q=q,stress=stress),
-                         Npaths=1e4,endtime=1, dt=1e-2){
+stressed_sim_CVaR <- function(kappa, jump_dist, stress_type = "CVaR",
+                         stress_parms = list(c=c,q=q,s=s,VaR_stress=VaR_stress,
+                                             CVaR_stress=CVaR_stress),
+                         Npaths=1e4,endtime=1,dt=1e-2){
 
   # first, draw from the jump size distribution
   Ndraws <- 1e4
@@ -24,11 +26,16 @@ stressed_sim <- function(kappa, jump_dist, stress_type = "VaR",
 
       Nsteps <- endtime / dt + 1   # number of steps to take
 
-      if (is.null(stress_parms$q) & !(is.null(stress_parms$stress))){
-        stress_parms$q <- stress_parms$stress * compute_VaR(kappa,stress_parms$c,jump_dist)
+      if (is.null(stress_parms$q) & !(is.null(stress_parms$VaR_stress))){
+        stress_parms$q <- stress_parms$VaR_stress *
+          compute_VaR(kappa=kappa,c=stress_parms$c,dist=jump_dist)
+      }
+      if (is.null(stress_parms$s) & !(is.null(stress_parms$CVaR_stress))){
+        stress_parms$q <- stress_parms$CVaR_stress *
+          compute_CVaR(kappa=kappa,q=stress_parms$q,c=stress_parms$c,dist=jump_dist)
       }
 
-      eta <- eta_VaR(kappa=kappa, stress_parms=stress_parms, dist=jump_dist)
+      eta <- eta_CVaR(kappa=kappa, stress_parms=stress_parms, dist=jump_dist)
 
       # create grid of Gs and kappas
       times <- seq(0,endtime,by=dt)
@@ -43,9 +50,9 @@ stressed_sim <- function(kappa, jump_dist, stress_type = "VaR",
 
       # compute G and kappa at grid points
       for (i in 1:Nsteps){
-        distorted <- sim_G_kappa(t=times[i],x=x_seq,eta=eta,kappa=kappa,
-                                 q=stress_parms$q,c=stress_parms$c,
-                                 dist=jump_dist,Draws=Draws)
+        distorted <- sim_G_kappa_CVaR(t=times[i],x=x_seq,eta=eta,kappa=kappa,
+                                      stress_parms=stress_parms,dist=jump_dist,
+                                      Draws=Draws)
         G_grid[i,] <- distorted$G_Q
         kappa_grid[i,] <- distorted$kappa_Q
       }
@@ -77,35 +84,38 @@ stressed_sim <- function(kappa, jump_dist, stress_type = "VaR",
 }
 
 
-#' compute G^Q and kappa^Q for Y ~ dist
+#' compute G^Q and kappa^Q for Y ~ dist for CVaR
 #' helper function for stressed_sim()
 #'
 #' @param t A number,time
 #' @param x A vector of numbers, state
 #' @param eta float
 #' @param kappa integer
-#' @param q float, distorted quantile
-#' @param c float, qantile to distort, c in (0,1)
+#' @param stress_parms list
 #' @param dist RPS_dist object
 #' @param Draws vector
 #' @param delta_y float, step size for Riemann integration
+#' @param y_max int, maximum for integration of h(t,x,y) along y-axes
+#' @param tol dbl, cut off integration when prob less than tol
 #'
 #' @return A list containing:
 #' 1) an vector of length(x), containing a draw from G^Q at for each x
 #' 2) a vector of length length(x) containing kappa^Q at each x
 #'
 #' @export
-sim_G_kappa <- function(t,x,eta,kappa,q,c,dist,Draws,delta_y=0.1){
-  with(dist, {
+sim_G_kappa_CVaR <- function(t,x,eta,kappa,stress_parms,dist,Draws,
+                        delta_y=0.1,y_max=100,tol=1e-10) {
+  with(c(dist,stress_parms), {
     # x is a vector
     xlen <- length(x)
-    y <- seq(0,q+5,by=delta_y)  # h is constant for x+y > q
+    y_max <- seq(1,y_max)[which(dens_fun(seq(1,y_max),parms) < tol)[1]]
+    y <- seq(0,y_max,by=delta_y)  # h is constant for x+y > q
     ylen <- length(y)
     h <- array(dim=c(xlen,ylen))
 
     # compute h at each y
     for (i in 1:ylen){
-      h[,i] <- h_VaR(x=x,y=y[i],t=t,dist=dist,eta=eta,q=q,c=c,kappa=kappa)
+      h[,i] <- h_CVaR(x=x,y=y[i],t=t,dist=dist,eta=eta,q=q,kappa=kappa)
     }
 
     # initialize
@@ -114,8 +124,7 @@ sim_G_kappa <- function(t,x,eta,kappa,q,c,dist,Draws,delta_y=0.1){
 
     for (j in 1:xlen){
       # compute integral (kappa.Q):
-      kappa_Q[j] <- dist_fun(0,parms) + delta_y * sum(h[j,] * dens_fun(y,parms)) +
-        tail(h[j,],1) * (1-dist_fun(tail(y,1),parms))
+      kappa_Q[j] <- delta_y * sum(h[j,] * dens_fun(y,parms))
       # draw from G.Q
       weights <- approx(x=y,y=h[j,],xout=Draws)$y / kappa_Q[j]    # compute weights
       G_Q[j] <- sample(Draws, 1, replace = TRUE, prob=weights)   # sample 1 from G^Q
@@ -125,31 +134,5 @@ sim_G_kappa <- function(t,x,eta,kappa,q,c,dist,Draws,delta_y=0.1){
   })
 }
 
-#' function to get path of X under original probability measure
-#'
-#' @param object RPS_model object
-#'
-#' @return matrix, paths of original model
-#' @export
-#'
-sim_baseline <- function(object){
-  with(object,{
-    # draw from the jump size distribution
-    withr::with_seed(720,{
-      Draws <- jump_dist$sim_fun(1e4,jump_dist$parms)
 
-      Npaths <- dim(paths)[2]
 
-      Nsteps <- length(time_vec)
-      X <- matrix(nrow=Nsteps,ncol=Npaths) # empty matrix to store results
-      X[1,] <- rep(0,Npaths)
-
-      for (i in 2:Nsteps){
-        U <- runif(Npaths)
-        dt <- time_vec[i] - time_vec[i-1]
-        X[i,] <- X[i-1,] + sample(Draws,Npaths) * as.integer(U < (1 - exp(-kappa * dt)))
-      }
-    })
-    return(X)
-  })
-}
